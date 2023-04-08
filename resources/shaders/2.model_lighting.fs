eb7be93d4390b30884f32b3ba3c7d2c14c72cb80
duki <mi19325@alas.matf.bg.ac.rs>
2023-04-08 05:14:06 +0200

    Added blinn-phong lightning

--- resources/shaders/2.model_lighting.fs
+++ resources/shaders/2.model_lighting.fs
@@ -35,18 +35,25 @@ vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir)
     float diff = max(dot(normal, lightDir), 0.0);
     // specular shading
     vec3 reflectDir = reflect(-lightDir, normal);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    // attenuation
-    float distance = length(light.position - fragPos);
-    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
-    // combine results
-    vec3 ambient = light.ambient * vec3(texture(material.texture_diffuse1, TexCoords));
-    vec3 diffuse = light.diffuse * diff * vec3(texture(material.texture_diffuse1, TexCoords));
-    vec3 specular = light.specular * spec * vec3(texture(material.texture_specular1, TexCoords).xxx);
-    ambient *= attenuation;
-    diffuse *= attenuation;
-    specular *= attenuation;
-    return (ambient + diffuse + specular);
+    vec3 halfwayVec = normalize(viewDir + lightDir);
+        float spec = pow(max(dot(Normal, halfwayVec), 0.0), material.shininess);
+
+        // attenuation
+        float distance = length(light.position - fragPos);
+        float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+        // combine results
+
+
+        vec3 ambient = light.ambient * vec3(texture(material.texture_diffuse1, TexCoords));
+        vec3 diffuse = light.diffuse * diff * vec3(texture(material.texture_diffuse1, TexCoords));
+        vec3 specular = light.specular * spec * vec3(texture(material.texture_specular1, TexCoords).xxx);
+        ambient *= attenuation;
+        diffuse *= attenuation;
+        specular *= attenuation;
+        if (diff != 0.0f) {
+            specular = vec3(0.0f);
+        }
+        return (ambient + diffuse + specular);
 }
 
 void main()
