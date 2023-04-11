554161bca9df35981b0cfcb99c5588e02de7faa9
duki <mi19325@alas.matf.bg.ac.rs>
2023-04-12 01:39:38 +0200

    Fixed depth testing

--- resources/shaders/2.model_lighting.fs
+++ resources/shaders/2.model_lighting.fs
@@ -56,11 +56,17 @@ vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir)
         return (ambient + diffuse + specular);
 }
 
-float near = 5.0f;
-float far = 10.0f;
+float near = 0.2005f;
+float far = 50.0f;
 
 float linearizeDepth(float depth) {
     return (2.0 * far * near) / (far + near - (depth * 2.0 - 1.0) * (far - near));
+}
+
+float logDepth(float depth, float steepness, float offset) {
+
+    float zVal = linearizeDepth(depth);
+    return (1 / (1 + exp(-steepness * (zVal - offset))));
 }
 
 void main()
@@ -68,5 +74,9 @@ void main()
     vec3 normal = normalize(Normal);
     vec3 viewDir = normalize(viewPosition - FragPos);
     vec3 result = CalcPointLight(pointLight, normal, FragPos, viewDir);
-    FragColor = vec4(result, 1.0); //* vec4(vec3(1.0) - vec3(linearizeDepth(gl_FragCoord.z) / far), 1.0f);
+    float depth = logDepth(gl_FragCoord.z, 0.1f, 0.2f);
+
+    FragColor = vec4(result, 1.0f) * (1.0 - depth) + vec4(depth * vec4(vec3(0.0085, 0.0085, 0.0090), 1.0));
+    // FragColor = vec4(vec3(linerizeDepth(gl_FragCoord.z) / far), 1.0f);
+    // FragColor = vec4(result, 1.0); //* vec4(vec3(1.0) - vec3(linearizeDepth(gl_FragCoord.z) / far), 1.0f);
 }
